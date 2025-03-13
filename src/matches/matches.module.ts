df23369bc5d7a52d0c15dad946ccb883d4c70ad6
Aleksey Kosourov <aleksey.kosourov@decathlon.com>
2025-03-13 22:41:36 +0300

    Правки ссылок

--- src/matches/matches.module.ts
+++ src/matches/matches.module.ts
@@ -2,9 +2,27 @@ import { Module } from '@nestjs/common';
 import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppConfig } from '../config/configuration';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService<AppConfig>) => {
+        const apiMatchesConfig =
+          configService.get<AppConfig['apiMatches']>('apiMatches');
+
+        return {
+          baseURL: apiMatchesConfig?.url,
+          headers: {
+            'X-Auth-Token': apiMatchesConfig?.token,
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MatchesController],
   providers: [MatchesService],
 })
